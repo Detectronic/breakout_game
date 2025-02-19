6a58c1f751ba63f4cac52b3377a7b257141c996c
Dave Smith <fishbulbfirmwareltd@gmail.com>
2025-02-19 09:29:45 +0000

    - Test

--- app.h
+++ app.h
@@ -12,7 +12,8 @@
  * www.silabs.com/about-us/legal/master-software-license-agreement. This
  * software is distributed to you in Source Code format and is governed by the
  * sections of the MSLA applicable to Source Code.
- *
+ *  THIS IS A TEST
+ 
  ******************************************************************************/
 
 #ifndef APP_H
